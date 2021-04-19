eeb653e558e6d12b60aea207eb706a7bdee4bcd1
Hockey86 <hockeysun86@gmail.com>
2021-04-18 23:59:49 -0400

    update step2

--- simulation/step2_fit_classifiers.py
+++ simulation/step2_fit_classifiers.py
@@ -1,3 +1,4 @@
+from collections import Counter
 import os
 import pickle
 import subprocess
@@ -47,8 +48,14 @@ def fit_model(X, y, Ncv, model_type='logreg', best_params=None, n_jobs=1, random
     else:
         params = best_params
         
+    # generate training and testing ids
     cv = StratifiedKFold(n_splits=Ncv, shuffle=True, random_state=random_state)
-    for cvi, (trid, teid) in enumerate(cv.split(X, y)):
+    trteid = [x for x in cv.split(X, y)]
+
+    # append last fold with training=all, testing=empty, so that the model is refit
+    trteid.append((np.arange(len(X)), []))
+
+    for cvi, (trid, teid) in enumerate(trteid):
         Xtr = X[trid]
         ytr = y[trid]
         
@@ -60,13 +67,13 @@ def fit_model(X, y, Ncv, model_type='logreg', best_params=None, n_jobs=1, random
             model = LogisticRegression(
                 penalty='elasticnet', class_weight='balanced',
                 random_state=random_state, solver='saga',
-                max_iter=1000, n_jobs=n_jobs)
-            model_params  = {'C':[0.1,1,10], 'l1_ratio':[0.5,0.6,0.7,0.8,0.9]}
+                max_iter=10000, n_jobs=n_jobs)
+            model_params  = {'C':[0.1,1,10], 'l1_ratio':np.arange(0.2,1,0.2)}
         elif model_type=='rf':
             model = RandomForestClassifier(
                 n_jobs=n_jobs, min_samples_leaf=10,
                 random_state=random_state, class_weight='balanced')
-            model_params = {'n_estimators':[10,50,100], 'max_depth':[3,5], 'ccp_alpha':[0.1,1,10]}
+            model_params = {'n_estimators':[5,10,50,100], 'max_depth':[3,5], 'ccp_alpha':[0.01,0.1,1]}
         else:
             raise ValueError('Unknown model_type: %s'%model_type)
             
@@ -84,17 +91,17 @@ def fit_model(X, y, Ncv, model_type='logreg', best_params=None, n_jobs=1, random
                     val = Counter([param[p] for param in params]).most_common()[0][0]
                     if '__' in p:
                         pp = p.split('__')
-                        exec('model.%s.%s = %f'%(pp[0], pp[1], val))  # TODO assumes two
+                        exec(f'model.{pp[0]}.{pp[1]} = {val}')  # TODO assumes max two levels
                     else:
-                        exec('model.%s = %f'%(p, val))
+                        exec(f'model.{p} = {val}')
                 
         elif len(params)>0:
             for p in params[cvi]:
                 if '__' in p:
                     pp = p.split('__')
-                    exec('model.%s.%s = %f'%(pp[0], pp[1], params[cvi][p]))  # TODO assumes two
+                    exec(f'model.{pp[0]}.{pp[1]} = {params[cvi][p]}')  # TODO assumes max two levels
                 else:
-                    exec('model.%s = %f'%(p, params[cvi][p]))
+                    exec(f'model.{p} = {params[cvi][p]}')
             if hasattr(model, 'n_jobs'):
                 model.n_jobs = n_jobs
         model.fit(Xtr, ytr)
@@ -139,26 +146,36 @@ if __name__=='__main__':
     random_state = 2021
     n_jobs = 8
     Ncv = 5
-    output_dir = 'step2_output_models_predictions'
+    output_dir = 'models_predictions'
+
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
     
-    import pdb;pdb.set_trace()
     for i in tqdm(range(len(df))):
         for n in Ns:
             # generate different training sizes
             df_sim = pd.read_csv(df.Path.iloc[i])
             df_sim = df_sim[:n]  # because datset is shuffled, taking first N approximately keeps the class ratio
-            #TODO assumes event is the last column
+            #TODO now assumes event is at the last column
             X = df_sim.values.astype(float)[:,:-1]
             y = df_sim.values[:,-1].astype(int)
             
             for model_type in model_types:
+                print(model_type)
                 cv_scores_tr, cv_scores_te, params, models, Xtes, ytes, yptes, yp = fit_model(X, y, Ncv, model_type=model_type, n_jobs=n_jobs, random_state=random_state)
                 model = models[-1]
+                yp = yp[:,1]  #TODO now assumes binary
                 
                 # save model
                 save_file_name = os.path.basename(df.Path.iloc[i]).replace('.csv',f'_model_N{n}_model{model_type}.pickle')
                 with open(os.path.join(output_dir, save_file_name), 'wb') as ff:
                     pickle.dump(model, ff)
+
+                # save performance
+                save_file_name = os.path.basename(df.Path.iloc[i]).replace('.csv',f'_perf_N{n}_model{model_type}.xlsx')
+                with pd.ExcelWriter(os.path.join(output_dir, save_file_name)) as ff:
+                    cv_scores_tr.to_excel(ff, sheet_name='training')
+                    cv_scores_te.to_excel(ff, sheet_name='testing')
                     
                 # save prediction
                 save_file_name = os.path.basename(df.Path.iloc[i]).replace('.csv',f'_prediction_N{n}_model{model_type}.csv')
